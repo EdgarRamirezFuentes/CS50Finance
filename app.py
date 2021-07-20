fefa173e2c8662bf100fb54ebdea17d233f08314
Edgar Alejandro Ramírez Fuentes <edgar-alejandro-fuentes@hotmail.com>
2021-07-20 16:01:19 -0500

    sessions updated

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")   
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
